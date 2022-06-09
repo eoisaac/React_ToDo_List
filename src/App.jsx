657679bee83f3daff54bb15a50c4b0217a7e7948
Isaac <eoisaacc@gmail.com>
2022-06-08 21:49:55 -0300

    feat: create new task form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import './global.css';
+import styles from './App.module.css';
 
 import { Header } from './components/Header';
+import { Input } from './components/Input';
+import { CreateButton } from './components/CreateButton';
 
 export const App = () => {
   return (
-		<>
+		<main className={styles.root}>
 			<Header />
-		</>
+			<div className={styles.container}>
+				<form className={styles.newTaskForm}>
+					<Input 
+						name="taskInpt" 
+						id="taskInpt" 
+						placeholder="Adicione uma nova tarefa" 
+					/>
+
+					<CreateButton />
+				</form>
+			</div>
+		</main>
 	);
 };
