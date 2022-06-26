54e49211a84f63740bd6f9d75b1ad92deeefd63d
Isaac <eoisaacc@gmail.com>
2022-06-25 22:52:16 -0300

    fix: fix Task component height

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { App } from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from './App';
 
 if (import.meta.hot) {
-  import.meta.hot.on(
-    "vite:beforeUpdate",
-    () => console.clear()
-  );
+  import.meta.hot.on('vite:beforeUpdate', () => console.clear());
 }
 
 ReactDOM.createRoot(document.querySelector('#root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
